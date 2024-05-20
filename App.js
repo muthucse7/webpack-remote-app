c3f41635265b3eb9eef587edb611439f34cfa09d
Muthu Selvam <muthu.softwar@gmail.com>
2024-05-20 10:03:58 -0400

    Updated the title to load remote APP

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello from Webpack React Host APP!</h1>
+      <h1>Hello from Webpack React Remote APP!</h1>
     </div>
   );
 }
