ad3c4266769c0e660ed8dc23c88d16dff98827c9
Muthu Selvam <muthu.softwar@gmail.com>
2024-05-19 22:33:39 -0400

    Webpack config added

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  mode: 'development',
-  entry: './index.js', // Assuming index.js is located in the root directory
+  mode: "development",
+  entry: "./index.js",
   output: {
-    publicPath: 'http://localhost:3000/', // The public URL of the output directory
-    filename: '[name].js', // Output filename, [name] will be replaced by the entry name
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    publicPath: "http://localhost:3000/",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    port: 3000, // Port number
+    port: 3000,
   },
   module: {
     rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
-      ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Assuming index.html is located in the root directory
+      template: "./index.html",
     }),
     new ModuleFederationPlugin({
-      name: 'remoteApp',
-      filename: 'remoteEntry.js',
+      name: "remoteApp",
+      library: { type: "var", name: "remoteApp" },
+      filename: "remoteEntry.js",
       exposes: {
-        './remoteApp': './index.js', // Expose your entry module
+        "./remoteApp": "./App.js", // Expose your entry module
+      },
+      remotes: {
+        // Define remote modules that the host application will consume
+        remoteApp: "remoteApp@http://localhost:3000/remoteEntry.js",
       },
     }),
   ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 };
